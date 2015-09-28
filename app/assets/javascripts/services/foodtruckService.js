d5f63c77ad493268ed11cc8033aece4385a60eed
facingsouth <xin.d.liu@gmail.com>
2015-09-28 15:20:57 -0700

    add food truck service

---
+++ app/assets/javascripts/services/foodtruckService.js
@@ -0,0 +1,22 @@
+ft.factory('foodtruckService', ['Restangular', function(Restangular) {
+  var foodTrucks = {};
+
+  function getFoodTrucks (lat, lon, radius) {
+    var kwargs = {
+      lat: lat,
+      lon: lon,
+      radius: radius
+    }
+    Restangular.all('foodtrucks').getList(kwargs).then(function(data){
+      console.log('get food trucks nearby: ', data);
+      foodtrucks.list = data;
+    }, function(error){
+      console.log('fail to get food trucks: ', error);
+    })
+  }
+
+  return {
+    foodtrucks: foodtrucks,
+    getFoodTrucks: getFoodTrucks
+  }
+}])
